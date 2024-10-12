d09f6896332928f10956f1c21369b9e83ce679e6
dong woo sin <109775028+dongsinwoo@users.noreply.github.com>
2024-10-13 02:21:55 +0900

    3-1 input field component

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,7 +1,14 @@
 const colors = {
     MAIN_700 : "#3C9BEE",
     MAIN_500 : "#8CC4F5",
-    WHITE: "#FFF"
+    GRAY_700 : "#575757",
+    GRAY_500 : "#8E8E8E",
+    GRAY_200 : "#E7E7E7",
+    RED_500: "FF5F5F",
+    RED_300 : "#FFB4B4",
+    BLACK: "#000",
+    WHITE: "#FFF",
+    
 }
 
 export {colors}
--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InputField from '../../components/InputField';
 
 
 function LoginScreen() {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState("");
+
+  // const handleChangeEmail = (text:string) =>{
+  //   setEmail(text)
+  // }
+  // const handleChangePassword = (text:string) => {
+  //   setPassword(text)
+  // }
+
+  const [value, setValue] = useState({
+    email : '',
+    password : ''
+  })
+  const [touched, setTouched] = useState({
+    email : false,
+    password : false
+  })
+
+  const handleChangeValue = (name: string, text:string)=>{
+    setValue({
+      ...value,
+      [name] : text
+    })
+  }
+  const handleBlur = (name:string) =>{
+    setTouched({
+      ...touched,
+      [name] : true
+    })
+  }
+  
   return (
-    <View>
-        <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style = {styles.container}>
+        <View style = {styles.inputContainer}>
+          <InputField 
+          placeholder='이메일' 
+          inputMode='email' 
+          error='이메일을 입력하세요'
+          touched = {touched.email}
+          value={value.email} 
+          onChangeText={(text)=> handleChangeValue("email",text)}
+          onBlur={()=>handleBlur("email")} />
+          <InputField 
+          placeholder='비밀번호' 
+          value={value.password} 
+          error='비밀번호를 입력하세요'
+          touched = {touched.password}
+          onChangeText={(text)=> handleChangeValue("password",text)} 
+          onBlur={()=>handleBlur("password")}
+          secureTextEntry/>
+        </View>
+    </SafeAreaView>
+    
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{
+    flex : 1,
+    margin: 30,
+  },
+  inputContainer:{
+    gap: 20,
+
+  }
+});
 
 export default LoginScreen;
